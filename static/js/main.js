9c2d44c2eb97673f88c8677dcc98c2bd31e80471
atanana <tanana@email.ru>
2017-12-23 14:46:48 +0300

    hide image tag when not needed

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,6 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
 var assertImg = document.getElementById('asset');
 
 socket.on('asset change', function (data) {
+    assertImg.style.display = data.name ? 'block' : 'none';
     assertImg.src = '/static/assets/' + data.name;
 });
